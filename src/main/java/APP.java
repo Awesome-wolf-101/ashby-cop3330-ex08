5481dae1e9c7c8909f69dff39c7187aa305a1bd3
Joshua Ashby <joshuaashby16@gmail.com>
2021-06-05 15:51:04 -0400

    Initial commit

---
+++ src/main/java/APP.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class APP {
+
+
+    public static void main(String[] args) {
+        System.out.println("How many people?");
+        Scanner sc = new Scanner(System.in);
+        String NumPeople = sc.nextLine();
+        System.out.println("How many pizzas do you have?");
+        String Pizzas = sc.nextLine();
+        System.out.println("How many slices per pizza?");
+        String SlicesPerPizza = sc.nextLine();
+
+        int NumPeopleInt = Integer.parseInt(NumPeople);
+        int PizzasInt = Integer.parseInt(Pizzas);
+        int SlicesPerPizzaInt = Integer.parseInt(SlicesPerPizza);
+        int TotalSlices = PizzasInt * SlicesPerPizzaInt;
+        System.out.println(NumPeopleInt + " people with "+ PizzasInt + " pizzas (" + TotalSlices + " slices)");
+
+
+        int PizzaPerHuman = TotalSlices / NumPeopleInt;
+        int LeftoverPizza = TotalSlices % NumPeopleInt;
+
+
+        System.out.println("Each person gets " + PizzaPerHuman + " pieces of pizza.");
+        System.out.println("There are " + LeftoverPizza + " leftover pieces.");
+
+
+
+    }
+}
